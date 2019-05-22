af7cda12262fe5af8be80d71933fa51669959b2b
Vanessa Eldoh <van.eldoh@gmail.com>
2019-05-22 17:12:47 +0200

    cocktail front

---
+++ app/javascript/components/banner.js
@@ -0,0 +1,11 @@
+import Typed from 'typed.js';
+
+const loadDynamicBannerText = () => {
+  new Typed('#typed', {
+    strings: ["ALLEEEEEEEEEEEEEEEEEZ LAAAAA !"],
+    typeSpeed: 50,
+    loop: true
+  });
+}
+
+export { loadDynamicBannerText };
